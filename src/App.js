1e5e411b37de504583d905bfd05b20d95a2b3ea7
Aravind-arvu <aravindarvu@gmail.com>
2022-09-21 13:16:39 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+const App=()=>{
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    )
+  }
+export default App;
